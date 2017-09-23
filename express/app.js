d2d1f68b8d29d9bc5b2c69787e64e04fc9cf2dc4
smahesh1 <smahesh3288@gmail.com>
2017-09-23 15:18:03 -0400

    Made express js stuff work

--- express/app.js
+++ express/app.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const app = express()
+const bodyParser= require('body-parser')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+//app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+  try{
+    //res.sendFile(__dirname + '/university.html')
   const contractArtifact = require('../build/contracts/CredentialStore.json')
   const truffleContractFactory = require('truffle-contract')
   const CredentialStore = truffleContractFactory(contractArtifact)
-  console.log(CredentialStore)
+  //console.log(CredentialStore)
+  	var students = [
+        { name: 'asdf', did_graduate: 'Y'},
+        { name: 'jkl;', did_graduate: 'N' }
+    ];
+
+    res.render('universite.ejs', {
+        students: students
+    });
+}
+catch(er){
+	console.log(er)
+}
+
 })
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+// app.post('/student', (req, res) => {
+//   console.log(req.body)
+// })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
