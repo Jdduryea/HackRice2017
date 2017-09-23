d57f7b0ae489df7a694ef0d05a90061023c92621
smahesh1 <smahesh3288@gmail.com>
2017-09-23 01:48:13 -0400

    Made changes for windows to truffle.js

--- webstuff/config.js
+++ webstuff/config.js
@@ -3,3 +3,5 @@ var web3_provider = 'http://localhost:8545';
 var _web3 = new web3();
 _web3.setProvider(new web3.providers.HttpProvider(web3_provider));
 exports.web3 = _web3;
+
+exports.contract_addr = 'add some hash key'
