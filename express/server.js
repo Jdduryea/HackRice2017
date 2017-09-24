17144846d26060490421b95cd6b61daea644d4bf
smahesh1 <smahesh3288@gmail.com>
2017-09-23 20:35:44 -0400

    Added the student ejs

--- express/server.js
+++ express/server.js
@@ -3,6 +3,8 @@ const app = express()
 const bodyParser= require('body-parser')
 //app.set('view engine', 'ejs')
 
+app.use(express.static(__dirname + '/static'));
+
 app.get('/', function(req, res){
   try{
 
@@ -14,7 +16,18 @@ catch(er){
 
 })
 
-app.use(express.static(__dirname + '/static'));
+app.get('/student', function(req, res){
+  try{
+
+    res.render('student.ejs');
+}
+catch(er){
+  console.log(er)
+}
+
+})
+
+
 
 app.get('/university', function(req, res){
   try{
@@ -46,6 +59,9 @@ app.post('/student', (req, res) => {
  console.log(req.body)
 })
 
+app.post('/send_info', (req, res) => {
+ console.log(req.body)
+})
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
