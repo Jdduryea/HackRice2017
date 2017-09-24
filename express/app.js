f05610f4ba89747003bec2352e492f83b12ae2b9
jackduryea <jd50@rice.edu>
2017-09-23 19:12:59 -0500

    updated ui

--- express/app.js
+++ express/app.js
@@ -1,42 +1,6 @@
 const express = require('express')
 const app = express()
-const bodyParser= require('body-parser')
 
-<<<<<<< HEAD
-//app.set('view engine', 'ejs')
-
-app.get('/', function(req, res){
-  try{
-    //res.sendFile(__dirname + '/university.html')
-  const contractArtifact = require('../build/contracts/CredentialStore.json')
-  const truffleContractFactory = require('truffle-contract')
-  const CredentialStore = truffleContractFactory(contractArtifact)
-  //console.log(CredentialStore)
-  	var students = [
-        { name: 'asdf', did_graduate: 'Y'},
-        { name: 'jkl;', did_graduate: 'N' }
-    ];
-
-    res.render('universite.ejs', {
-        students: students
-    });
-}
-catch(er){
-	console.log(er)
-}
-
-})
-
-
-app.use(bodyParser.urlencoded({extended: true}))
-
-// app.post('/student', (req, res) => {
-//   console.log(req.body)
-// })
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-=======
 const fs = require('fs')
 const config = JSON.parse(fs.readFileSync('../config.json'));
 
@@ -59,5 +23,4 @@ app.get('/', function (req, res) {
 
 app.listen(config.ports.express, function () {
   console.log('Example app listening on port '+config.ports.express+'!')
->>>>>>> 45276af0e2b9a44fd125078d37fc9fa8e953a8cb
 })
